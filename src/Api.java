65c56997151508cd5ae37f3c0bfcc45b2a1cfc80
Al-Mansori <11410120210729@stud.cu.edu.eg>
2023-11-09 19:32:02 +0200

    merge

--- src/Api.java
+++ src/Api.java
@@ -1,8 +1,17 @@
 import java.util.List;
 
 public class Api {
-    private List<String> tt;
+    private List<String> acountBackList;
+
     public Api() {
-                 
+
+    }
+
+    public void add(String acountNumber) {
+        acountBackList.add(acountNumber);
+    }
+
+    public List<String> getAcountBackList() {
+        return acountBackList;
     }
 }
