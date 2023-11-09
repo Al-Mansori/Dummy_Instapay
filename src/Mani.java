8916e79f2c3f8437293a842211dafae5d848fcc1
Mohammed Al-Mansori <95043517+Al-Mansori@users.noreply.github.com>
2023-11-09 13:27:48 +0200

    Update Mani.java

--- src/Mani.java
+++ src/Mani.java
@@ -3,17 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        Terminal terminal = new Terminal();
-        Scanner scanner = new Scanner(System.in);
-        while (true) {
-            System.out.print(terminal.getCurrentDirectory() + ":$ ");
-            String input = scanner.nextLine();
-
-            if (input.equalsIgnoreCase("exit")) {
-                break;
-            }
-            terminal.chooseCommandAction(input);
-        }
-        scanner.close();
+       
+      
     }
 }
